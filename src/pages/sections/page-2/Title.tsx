6f9b3b55fdd020a6ce1e70e5dba16ed7e0f33ad3
Zhafran Dzaky <zhafrandzaky@gmail.com>
2023-02-24 01:40:40 +0700

    update

--- src/pages/sections/page-2/Title.tsx
+++ src/pages/sections/page-2/Title.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const Title = () => {
   return (
-    <div className="flex flex-col justify-center min-h-[6rem] pt-14 sm:pt-16 pb-6 ">
-      <h1 className="text-center text-4xl font-bold s:text-4xl lg:text-5xl mb-1 md:mb-2">
+    <div className="flex flex-col justify-center items-center min-h-[6rem] pt-14 sm:pt-16 pb-6 ">
+      <h1 className="text-center text-3xl max-w-[200px] s:max-w-none font-bold s:text-4xl lg:text-5xl mb-1 md:mb-2">
         Discover your dream places
       </h1>
-      <p className="text-sm s:text-base text-center lg:text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <p className="text-sm s:text-base text-center max-w-[250px] s:max-w-none lg:text-lg">
+        Lorem ipsum dolor sit amet consectetur{" "}
+        <span className="hidden s:inline">adipisicing elit.</span>
       </p>
     </div>
   );
